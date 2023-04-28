d17ee1e8ae2bff9a12006f96996c8e33dab537c8
Sufiyan Rahmat <sufiyan@incube8.sg>
2023-04-28 17:28:42 +0800

    Sign JWT

---
+++ types/lesgo/utils/jwt/sign.d.ts
@@ -0,0 +1,7 @@
+declare function sign(
+    payload: {},
+    secret: string,
+    opts?: { algorithm?: string; expiresIn?: string | number; notBefore?: string | number },
+): Promise<string>;
+
+export default sign;
